916e093b8eefa8dcfc9dc6c300dec6b661c2407d
sr2026 <145223842+sr2026@users.noreply.github.com>
2025-04-09 18:49:19 +0530

    Add client/tailwind.config.js 1744204759647

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2563EB',
+          light: '#3B82F6',
+          dark: '#1D4ED8'
+        },
+        secondary: {
+          DEFAULT: '#6B7280',
+          light: '#9CA3AF',
+          dark: '#4B5563'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
